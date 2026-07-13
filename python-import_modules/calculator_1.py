2eefbeae6b1d0d32c5cc8b7a347ce38c2e82c91f
Nvawo Alamar <nyawoalamar5@gmail.com>
2026-07-13 09:59:34 +0300

    Add task 1 calculation program

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Returns the addition of a and b."""
+    return a + b
+
+
+def sub(a, b):
+    """Returns the subtraction of a and b."""
+    return a - b
+
+
+def mul(a, b):
+    """Returns the multiplication of a and b."""
+    return a * b
+
+
+def div(a, b):
+    """Returns the integer division of a and b."""
+    return int(a / b)
