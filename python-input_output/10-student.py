3b37c611d15afdf2bcbe390e10cc9cba0070df6c
Nvawo Alamar <nyawoalamar5@gmail.com>
2026-07-30 11:43:45 +0300

    Task 10: Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
+class Student:
+    """Define a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return the dictionary representation of the Student.
+
+        If attrs is a list of strings, only the specified
+        attributes are returned.
+        """
+        if isinstance(attrs, list) and all(
+                isinstance(attr, str) for attr in attrs):
+            return {
+                attr: self.__dict__[attr]
+                for attr in attrs
+                if attr in self.__dict__
+            }
+        return self.__dict__
