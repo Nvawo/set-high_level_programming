e8562fc31326198dde42ea5f79b53a3987703a86
Nvawo Alamar <nyawoalamar5@gmail.com>
2026-08-05 14:47:44 +0300

    Add CSV serialization and deserialization

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,10 +1,12 @@
+```python
 #!/usr/bin/python3
 """Defines the Base class."""
 import json
+import csv
 
 
 class Base:
-    """Base class for all other classes."""
+    """Base class for the project."""
 
     __nb_objects = 0
 
@@ -18,10 +20,23 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return JSON string representation of a list of dictionaries."""
+        """Return the JSON string representation of a list of dictionaries."""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON representation of objects to a file."""
+        filename = cls.__name__ + ".json"
+
+        if list_objs is None:
+            list_objs = []
+
+        list_dicts = [obj.to_dictionary() for obj in list_objs]
+
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(cls.to_json_string(list_dicts))
 
     @staticmethod
     def from_json_string(json_string):
@@ -31,32 +46,9 @@ class Base:
         return json.loads(json_string)
 
     @classmethod
-    def save_to_file(cls, list_objs):
-        """Write JSON representation of objects to a file."""
-        filename = cls.__name__ + ".json"
-
-        if list_objs is None:
-            list_objs = []
-
-        list_dictionaries = [
-            obj.to_dictionary() for obj in list_objs
-        ]
-
-        with open(filename, "w", encoding="utf-8") as file:
-            file.write(cls.to_json_string(list_dictionaries))
-
-    @classmethod
     def create(cls, **dictionary):
-        """Create an instance and set its attributes from a dictionary."""
-        if cls.__name__ == "Rectangle":
-            dummy = cls(1, 1)
-        elif cls.__name__ == "Square":
-            dummy = cls(1)
-        else:
-            raise TypeError("Unknown class")
-
-        dummy.update(**dictionary)
-        return dummy
+        """Return an instance with attributes set from a dictionary."""
+        raise NotImplementedError
 
     @classmethod
     def load_from_file(cls):
@@ -65,13 +57,79 @@ class Base:
 
         try:
             with open(filename, "r", encoding="utf-8") as file:
-                json_string = file.read()
+                list_dicts = cls.from_json_string(file.read())
         except FileNotFoundError:
             return []
 
-        list_dictionaries = cls.from_json_string(json_string)
+        return [cls.create(**data) for data in list_dicts]
 
-        return [
-            cls.create(**dictionary)
-            for dictionary in list_dictionaries
-        ]
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serialize objects to CSV."""
+        filename = cls.__name__ + ".csv"
+
+        with open(filename, "w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+
+            if list_objs is None:
+                return
+
+            if cls.__name__ == "Rectangle":
+                for obj in list_objs:
+                    writer.writerow([
+                        obj.id,
+                        obj.width,
+                        obj.height,
+                        obj.x,
+                        obj.y
+                    ])
+
+            elif cls.__name__ == "Square":
+                for obj in list_objs:
+                    writer.writerow([
+                        obj.id,
+                        obj.size,
+                        obj.x,
+                        obj.y
+                    ])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserialize objects from CSV."""
+        filename = cls.__name__ + ".csv"
+
+        try:
+            with open(filename, "r", newline="", encoding="utf-8") as file:
+                reader = csv.reader(file)
+
+                if cls.__name__ == "Rectangle":
+                    return [
+                        cls.create(
+                            id=int(row[0]),
+                            width=int(row[1]),
+                            height=int(row[2]),
+                            x=int(row[3]),
+                            y=int(row[4])
+                        )
+                        for row in reader
+                        if row
+                    ]
+
+                if cls.__name__ == "Square":
+                    return [
+                        cls.create(
+                            id=int(row[0]),
+                            size=int(row[1]),
+                            x=int(row[2]),
+                            y=int(row[3])
+                        )
+                        for row in reader
+                        if row
+                    ]
+
+        except FileNotFoundError:
+            return []
+
+        return []
+```
+
