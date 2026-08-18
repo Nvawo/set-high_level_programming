45877408e1f6040a7544c199663d908f2c31e3b5
Nvawo <nyawo@LAPTOP-FAMILY.localdomain>
2026-08-18 13:08:44 +0300

    Add requests POST email script

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Sends an email as a POST parameter and displays the response."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    response = requests.post(sys.argv[1], data={"email": sys.argv[2]})
+    print(response.text)
