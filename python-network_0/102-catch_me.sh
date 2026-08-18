01e20c2f68f8f131a289bddad2e8ccfa527d4825
Nvawo <nyawo@LAPTOP-FAMILY.localdomain>
2026-08-18 11:37:27 +0300

    Fix catch me request method

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to the catch_me endpoint and displays the server response.
-curl -s 0.0.0.0:5000/catch_me
+# Sends a PUT request to the catch_me endpoint and displays the server response.
+curl -s -X PUT 0.0.0.0:5000/catch_me
