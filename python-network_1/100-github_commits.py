855b5664fe6171b260a64afbf61ed4182242ab5c
Nvawo <nyawo@LAPTOP-FAMILY.localdomain>
2026-08-18 13:21:18 +0300

    Add GitHub commits script

---
+++ python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lists the 10 most recent commits of a GitHub repository."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+    response = requests.get(url)
+    commits = response.json()
+
+    for commit in commits[:10]:
+        sha = commit.get("sha")
+        author = commit.get("commit").get("author").get("name")
+        print("{}: {}".format(sha, author))
